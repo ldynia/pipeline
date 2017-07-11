f69c2b18db9f61a8fdb71e0e5c2e1e10248332fe
ldynia <ludd@cbs.dtu.dk>
2017-07-11 18:32:51 +0200

    adding docker-compose if to alpine

--- docker/alpine/startup.sh
+++ docker/alpine/startup.sh
@@ -1,4 +1,25 @@
 #!/bin/sh
 
+# Execute logic when image is run by docker-compose
+if [ ! -z "$DOCKER_COMPOSE" ]; then
+  # Add piep/container to known_hosts by looping over evry pipe/containers
+  for pipe in $(echo $PIPES | tr "," "\n")
+  do
+    # File exist
+    if [ -f /root/.ssh/known_hosts ]; then
+      # Is pipe to be found in known_hosts
+      if grep -q $pipe /root/.ssh/known_hosts ; then
+        echo "Do noting for pipe found in known_hosts"
+      else
+        echo "Pipe not found -add pipe to known_hosts"
+        ssh-keyscan  "$pipe" >> /root/.ssh/known_hosts
+      fi
+    else
+      echo "File does not exist -create file with pipe"
+      ssh-keyscan  "$pipe" >> /root/.ssh/known_hosts
+    fi
+  done
+fi
+
 echo "Start ssh deamon in foreground"
 /usr/sbin/sshd -D
