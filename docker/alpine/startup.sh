d56fa46a9ed8e05b3ea2f5f10cccb06d9babbff6
ldynia <ludd@cbs.dtu.dk>
2017-07-12 16:13:01 +0200

    updating submodules and regenerating ssh keys

--- docker/alpine/startup.sh
+++ docker/alpine/startup.sh
@@ -2,26 +2,23 @@
 
 # Execute logic when image is run by docker-compose
 if [ ! -z "$DOCKER_COMPOSE" ]; then
+  # Remove known_hosts
+  if [ -f /root/.ssh/known_hosts ] && [ -f /root/.ssh/authorized_keys ]; then
+    rm /root/.ssh/known_hosts
+    rm /root/.ssh/authorized_keys
+  fi
+
+  # Add pipline's id key to authorized_keys
+  cp /root/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+
   # Add piep/container to known_hosts by looping over evry pipe/containers
   for pipe in $(echo $PIPES | tr "," "\n")
   do
-    # File exist
-    if [ -f /root/.ssh/known_hosts ]; then
-      # Is pipe to be found in known_hosts
-      if grep -q $pipe /root/.ssh/known_hosts ; then
-        echo "Do noting for pipe found in known_hosts"
-      else
-        echo "Pipe not found -add pipe to known_hosts"
-        ssh-keyscan  "$pipe" >> /root/.ssh/known_hosts
-      fi
-    else
-      echo "File does not exist -create file with pipe"
-      ssh-keyscan  "$pipe" >> /root/.ssh/known_hosts
-    fi
+    ssh-keyscan  "$pipe" >> /root/.ssh/known_hosts
   done
 fi
 
-echo "Start ssh deamon in foreground"
+echo "Ssh deamon started in foreground"
 /usr/sbin/sshd -D
 
 exec "$@"
