5cae2d00b7bd0b3c1f640bbd14506e7f007b2ed4
ldynia <ludd@cbs.dtu.dk>
2017-08-11 13:04:30 +0200

    refactoring startup.sh

--- docker/ubuntu/startup.sh
+++ docker/ubuntu/startup.sh
@@ -2,9 +2,15 @@
 
 # Execute logic when image is run by docker-compose
 if [ ! -z "$DOCKER_COMPOSE" ]; then
-  # Remove known_hosts and authorized_keys
-  if [ -f /root/.ssh/known_hosts ] && [ -f /root/.ssh/authorized_keys ]; then
+  # Remove known_hosts
+  if [ -f /root/.ssh/known_hosts ]; then
+    echo "Removing '/root/.ssh/known_hosts'"
     rm /root/.ssh/known_hosts
+  fi
+
+  # Remove authorized_keys
+  if [ -f /root/.ssh/authorized_keys ]; then
+    echo "Removing '/root/.ssh/authorized_keys'"
     rm /root/.ssh/authorized_keys
   fi
 
@@ -14,6 +20,7 @@ if [ ! -z "$DOCKER_COMPOSE" ]; then
   # Add piep/container to known_hosts by looping over evry pipe/containers
   for pipe in $(echo $PIPES | tr "," "\n")
   do
+    echo "Adding '$pipe'' to '/root/.ssh/known_hosts'"
     ssh-keyscan  "$pipe" >> /root/.ssh/known_hosts
   done
 fi
